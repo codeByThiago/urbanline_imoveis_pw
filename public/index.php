48bddb711f6e68929ba5b1bcfbee67292364fbf7
Thiago Rodrigues <thiagorodriguess2007@gmail.com>
2025-09-08 15:35:50 -0300

    Delete public directory

--- public/index.php
+++
@@ -1,6 +0,0 @@
-<?php include("../src/includes/header.php");?>
-<?php include("../src/includes/navbar.php");?>
-
-<?php include("../src/view/home-view.php");?>
-
-<?php include("../src/includes/footer.php");?>
